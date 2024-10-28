b20fd9fb3da82bcfcec764a65bef5d39feedd096
Alkandro <alkandro@hotmail.com>
2024-10-28 23:08:01 +0900

    blureffectok

--- App.js
+++ App.js
@@ -1,7 +1,160 @@
+// import React, { useEffect, useState } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { createDrawerNavigator } from "@react-navigation/drawer";  // Importa el Drawer Navigator
+// import { ActivityIndicator, View } from "react-native";
+// import Icon from "react-native-vector-icons/Ionicons";
+// import LoginScreen from "./Login";
+// import CheckInScreen from "./CheckInScreen";
+// import AttendanceHistoryScreen from "./AttendanceHistoryScreen";
+// import RegisterScreen from "./RegisterScreen";
+// import UserProfileScreen from "./UserProfileScreen";
+// import UserListScreen from "./UserListScreen";
+// import Information from "./Information";
+// import { auth, db } from "./firebase";
+// import { doc, getDoc } from "firebase/firestore";
+// import 'react-native-gesture-handler';
+// import 'react-native-reanimated';
+
+// const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator(); // Define el Drawer Navigator
+
+// const UserBottomTabs = () => {
+//   return (
+//     <Tab.Navigator
+//       screenOptions={({ route }) => ({
+//         tabBarIcon: ({ color, size }) => {
+//           let iconName;
+//           if (route.name === "CheckIn") {
+//             iconName = "checkmark-circle-outline";
+//           } else if (route.name === "AttendanceHistory") {
+//             iconName = "time-outline";
+//           } else if (route.name === "UserProfile") {
+//             iconName = "person-outline";
+//           }
+//           return <Icon name={iconName} size={size} color={color} />;
+//         },
+//         tabBarActiveTintColor: "blue",
+//         tabBarInactiveTintColor: "gray",
+//       })}
+//     >
+//       <Tab.Screen name="CheckIn" component={CheckInScreen} options={{ title: "Check In" }} />
+//       <Tab.Screen 
+//       name="AttendanceHistory" 
+//       component={AttendanceHistoryScreen} 
+//       options={{ title: "Historial de Asistencia", headerTitleAlign: "center" }}
+     
+      
+//        />
+//       <Tab.Screen name="UserProfile" component={UserProfileScreen} options={{ title: "Perfil" }} />
+//     </Tab.Navigator>
+//   );
+// };
+// // Configuración del Drawer Navigator
+// const AppDrawer = () => (
+//   <Drawer.Navigator initialRouteName="Home">
+//     <Drawer.Screen name="Home" component={UserBottomTabs} />
+//     <Drawer.Screen name="Monthly Report" component={AttendanceHistoryScreen} />
+//     {/* Aquí puedes agregar más pantallas para los meses u otras propiedades */}
+//   </Drawer.Navigator>
+// );
+
+// const App = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [user, setUser] = useState(null);
+//   const [role, setRole] = useState(null);
+
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged(async (authUser) => {
+//       if (authUser) {
+//         setUser(authUser);
+//         const userDocRef = doc(db, "users", authUser.uid);
+//         const userDoc = await getDoc(userDocRef);
+//         if (userDoc.exists()) {
+//           setRole(userDoc.data().role);
+//         }
+//       } else {
+//         setUser(null);
+//         setRole(null);
+//       }
+//       setIsLoading(false);
+//     });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   if (isLoading) {
+//     return (
+//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//         <ActivityIndicator size="large" color="#0000ff" />
+//       </View>
+//     );
+//   }
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName={user ? (role === "admin" ? "UserList" : "UserTabs") : "Login"}>
+//         {!user ? (
+//           <>
+//             <Stack.Screen
+//               name="Login"
+//               component={LoginScreen}
+//               options={{
+//                 headerShown: false,
+//               }}
+//             />
+//             <Stack.Screen name="Information" component={Information} />
+//             <Stack.Screen
+//               name="Register"
+//               component={RegisterScreen}
+//               options={{
+//                 title: "Registro de Usuario",
+//                 headerTitleAlign: "center",
+//                 headerStyle: { backgroundColor: "white" },
+//                 headerTintColor: "black",
+//                 headerTitleStyle: { fontWeight: "bold" },
+//               }}
+//             />
+//           </>
+//         ) : role === "admin" ? (
+//           <>
+//             <Stack.Screen name="UserList" component={UserListScreen} />
+            
+//             <Stack.Screen 
+//             name="UserProfile" 
+//             component={UserProfileScreen}
+//             options={{
+//               title: "User Profile",
+//               headerTitleAlign: "center",
+//               headerStyle: { backgroundColor: "white" },
+//               headerTintColor: "black",
+//               headerTitleStyle: { fontWeight: "bold" },
+//             }} />
+//           </>
+//         ) : (
+//           <>
+//           <Stack.Screen name="Drawer" component={AppDrawer} options={{ headerShown: false }} />  
+//           <Stack.Screen
+//             name="UserTabs"
+//             component={UserBottomTabs}
+//             options={{ headerShown: false }}
+//           />
+//           </>
+//         )}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer"; 
 import { ActivityIndicator, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import LoginScreen from "./Login";
@@ -16,38 +169,37 @@ import { doc, getDoc } from "firebase/firestore";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
-const UserBottomTabs = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-          if (route.name === "CheckIn") {
-            iconName = "checkmark-circle-outline";
-          } else if (route.name === "AttendanceHistory") {
-            iconName = "time-outline";
-          } else if (route.name === "UserProfile") {
-            iconName = "person-outline";
-          }
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "blue",
-        tabBarInactiveTintColor: "gray",
-      })}
-    >
-      <Tab.Screen name="CheckIn" component={CheckInScreen} options={{ title: "Check In" }} />
-      <Tab.Screen 
-      name="AttendanceHistory" 
-      component={AttendanceHistoryScreen} 
-      options={{ title: "Historial de Asistencia", headerTitleAlign: "center" }}
-     
-      
-       />
-      <Tab.Screen name="UserProfile" component={UserProfileScreen} options={{ title: "Perfil" }} />
-    </Tab.Navigator>
-  );
-};
+// Tabs de navegación para CheckIn, AttendanceHistory, y UserProfile
+const UserBottomTabs = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
+        if (route.name === "CheckIn") iconName = "checkmark-circle-outline";
+        else if (route.name === "AttendanceHistory") iconName = "time-outline";
+        else if (route.name === "UserProfile") iconName = "person-outline";
+        return <Icon name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: "blue",
+      tabBarInactiveTintColor: "gray",
+    })}
+  >
+    <Tab.Screen name="CheckIn" component={CheckInScreen} options={{ title: "Check In", headerTitleAlign: "center" }} />
+    <Tab.Screen name="AttendanceHistory" component={AttendanceHistoryScreen} options={{ title: "Historial de Asistencia", headerTitleAlign: "center" }} />
+    <Tab.Screen name="UserProfile" component={UserProfileScreen} options={{ title: "Perfil", headerTitleAlign: "center" }} />
+  </Tab.Navigator>
+);
+
+// Drawer principal que contiene las pestañas y otras pantallas
+const AppDrawer = () => (
+  <Drawer.Navigator initialRouteName="UserTabs">
+    <Drawer.Screen name="UserTabs" component={UserBottomTabs} options={{ title: "Inicio", headerTitleAlign: "center" }} />
+    <Drawer.Screen name="Information" component={Information} options={{ title: "Información" }} />
+    {/* Aquí puedes agregar más pantallas si es necesario */}
+  </Drawer.Navigator>
+);
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -60,9 +212,7 @@ const App = () => {
         setUser(authUser);
         const userDocRef = doc(db, "users", authUser.uid);
         const userDoc = await getDoc(userDocRef);
-        if (userDoc.exists()) {
-          setRole(userDoc.data().role);
-        }
+        if (userDoc.exists()) setRole(userDoc.data().role);
       } else {
         setUser(null);
         setRole(null);
@@ -83,17 +233,25 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={user ? (role === "admin" ? "UserList" : "UserTabs") : "Login"}>
+      <Stack.Navigator initialRouteName={user ? (role === "admin" ? "UserList" : "Drawer") : "Login"}>
         {!user ? (
           <>
             <Stack.Screen
               name="Login"
               component={LoginScreen}
-              options={{
-                headerShown: false,
-              }}
+              options={{ headerShown: false }}
             />
-            <Stack.Screen name="Information" component={Information} />
+            <Stack.Screen 
+            name="Information" 
+            component={Information}
+            options={{
+              title: "Information",
+              headerTitleAlign: "center",
+              headerStyle: { backgroundColor: "black" },
+              headerTintColor: "white",
+              headerTitleStyle: { fontWeight: "bold" },
+            }} 
+            />
             <Stack.Screen
               name="Register"
               component={RegisterScreen}
@@ -109,24 +267,20 @@ const App = () => {
         ) : role === "admin" ? (
           <>
             <Stack.Screen name="UserList" component={UserListScreen} />
-            
-            <Stack.Screen 
-            name="UserProfile" 
-            component={UserProfileScreen}
-            options={{
-              title: "User Profile",
-              headerTitleAlign: "center",
-              headerStyle: { backgroundColor: "white" },
-              headerTintColor: "black",
-              headerTitleStyle: { fontWeight: "bold" },
-            }} />
+            <Stack.Screen
+              name="UserProfile"
+              component={UserProfileScreen}
+              options={{
+                title: "User Profile",
+                headerTitleAlign: "center",
+                headerStyle: { backgroundColor: "white" },
+                headerTintColor: "black",
+                headerTitleStyle: { fontWeight: "bold" },
+              }}
+            />
           </>
         ) : (
-          <Stack.Screen
-            name="UserTabs"
-            component={UserBottomTabs}
-            options={{ headerShown: false }}
-          />
+          <Stack.Screen name="Drawer" component={AppDrawer} options={{ headerShown: false,headerTitleAlign: "center" }} />
         )}
       </Stack.Navigator>
     </NavigationContainer>
--- Information.js
+++ Information.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import { View, Text, FlatList,Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { BlurView } from "expo-blur";
+import backgroundImage from "./assets/fotos/TashiroBlack.jpg";
 
 const Information = () => {
-return (
-    <View>
-      <Text>Lista de Usuarios</Text>
-    
-        
-        
-          <View>
-            <Text>Nombre: </Text>
-            <Text>Email:</Text>
-            <Text>Teléfono:</Text>
-            <Text>Apellido: </Text>
-            <Text>Check-ins:</Text>
-           
-          </View>
-        
-      
-      
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        {/* BlurView que ocupa el 80% de la altura de la pantalla */}
+        <BlurView instnsity={80} style={styles.absolute}>
+         
+          <Text style={styles.titulo1}>Direccion</Text>
+          <Text style={styles.titulo}>Teléfono de contacto</Text>
+          <Text style={styles.titulo}>Profesor</Text>
+          <Text style={styles.titulo}>
+            Días y horarios de entrenamiento para adultos
+          </Text>
+          <Text style={styles.titulo}>
+            Días y horarios de entrenamiento para niños
+          </Text>
+          <Text style={styles.titulo}>Check-ins:</Text>
+        </BlurView>
+      </ImageBackground>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  background: {
+    height: '80%',
+    marginTop:100, // Ocupa todo el contenedor
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  absolute: {
+    position: 'absolute',
+    top:5,
+    left: 10,
+    right: 10,
+    bottom: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius:5 , // Bordes redondeados (opcional)
+    borderWidth:5,
+    borderColor:"white",
+    height: '80%',
+  },
+  titulo: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 8,
+    textAlign: "center",
+    letterSpacing: 0.5,
+    textDecorationLine: "underline",
+  },
+  titulo1: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 8,
+    textAlign: "center",
+    letterSpacing: 0.5,
+    textDecorationLine: "underline",
+    marginTop: -170,
+  },
+});
 
 export default Information;
